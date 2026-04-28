197e2286f4e646fbbf504d2cbfa2d3bb8fca0287
PheonBest <antoninmarxer@orange.fr>
2026-04-28 23:18:16 +0200

    fix: set as immutable

--- live/headlamp-plugins/aws/main.tf
+++ live/headlamp-plugins/aws/main.tf
@@ -83,7 +83,7 @@ module "aws-federation-oidc" {
 
 resource "aws_ecr_repository" "this" {
   name                 = local.image_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
